c80fb174dcb50f24a78353ef4f9af8ba1410adf5
MSUDENOC <readjustee@mail.ru>
2024-05-23 22:16:19 +0300

    post request

--- src/test/java/tests/TestRegres.java
+++ src/test/java/tests/TestRegres.java
@@ -10,43 +10,48 @@ import static utils.Steps.*;
 
 public class TestRegres {
 
-   @BeforeSuite
-    public void setup(){
+    @BeforeSuite
+    public void setUp() {
         baseURI = "https://reqres.in/api";
     }
 
     @Test
-    public void testTestNG() {
+    public void testListUsers() {
+
         String url = "/users?page=2";
 
         Response response = GET(url);
 
         isStatusCodeValid(response, 200);
 
-
     }
+
     @Test
-    public void testSingleUser(){
+    public void testSingleUser() {
+
         String url = "/users/2";
 
         Response response = GET(url);
 
         isStatusCodeValid(response, 200);
 
+        isBodyContainsValue(response, "data.first_name", "Janet");
     }
 
     @Test
-    public void testCreate(){
+    public void testCreate() {
         String url = "/users";
 
         String body = "{\n" +
                 "    \"name\": \"morpheus\",\n" +
                 "    \"job\": \"leader\"\n" +
                 "}";
+
         Response response = POST(body, url);
 
         isStatusCodeValid(response, 201);
 
-    }
+        isBodyContains(response, "id");
 
+    }
 }
--- src/test/java/utils/Steps.java
+++ src/test/java/utils/Steps.java
@@ -3,14 +3,17 @@ package utils;
 import io.qameta.allure.Allure;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
-import static io.restassured.RestAssured.*;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class Steps {
-    @Step
-    public static Response GET(String endpoint){
 
-        Allure.addAttachment("URL", baseURI + endpoint);
+    @Step
+    public static Response GET(String endpoint) {
+
+        Allure.addAttachment("URL:", baseURI + endpoint);
 
         Response response = get(endpoint);
 
@@ -21,26 +24,34 @@ public class Steps {
         return response;
     }
 
-
     @Step
     public static void isStatusCodeValid(Response response, int expectedStatusCode) {
         response.then().assertThat().statusCode(expectedStatusCode);
-
     }
 
-    public static Response POST(String body, String endpoint){
+    @Step
+    public static Response POST(String body, String endpoint) {
 
         Allure.addAttachment("URL", baseURI + endpoint);
 
-        Allure.addAttachment("Request body ", body);
+        Allure.addAttachment("Request body", body);
 
-        Response response =  given().body(body).post(endpoint);
+        Response response = given().body(body).post(endpoint);
 
         Allure.addAttachment("Status Code", String.valueOf(response.statusCode()));
 
-        Allure.addAttachment("Response Body", response.body().prettyPrint());
+        Allure.addAttachment("Response body", response.body().prettyPrint());
 
         return response;
     }
 
+    @Step
+    public static void isBodyContainsValue(Response response, String key, String expectedResult) {
+        response.then().assertThat().body(key, equalTo(expectedResult));
+    }
+
+    @Step
+    public static void isBodyContains(Response response, String expectedResult) {
+        response.then().assertThat().body(containsString(expectedResult));
+    }
 }
